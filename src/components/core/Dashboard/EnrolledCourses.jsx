be92a4fcab398168cf0f2fcdf86c7d16a57618b5
AnishPowar01 <powaranish74@gmail.com>
2023-07-30 17:35:46 +0530

    Added Dashboard Page

---
+++ src/components/core/Dashboard/EnrolledCourses.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const EnrolledCourses = () => {
+    // backend call
+
+    
+  return (
+    <div>
+        <div>
+        EnrolledCourses
+        </div>
+    </div>
+  )
+}
+
+export default EnrolledCourses
---
+++ src/components/core/Dashboard/Settings/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ChangeProfilePicture from "./ChangeProfilePicture"
+import EditProfile from "./EditProfile"
+import UpdatePassword from "./UpdatePassword"
+import DeleteAccount from "./DeleteAccount"
+
+
+const Settings = () => {
+  return (
+    <>
+
+    <h1 className='mb-14 text-3xl font-medium text-richblack-5'>
+        Edit Profle
+    </h1>
+
+    <ChangeProfilePicture/>
+
+    <EditProfile/>
+
+    <UpdatePassword/>
+
+    <DeleteAccount/>
+
+    </>
+  )
+}
+
+export default Settings
