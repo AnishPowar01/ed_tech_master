f612eb725dc80a22ae03c105ddaf35ae260d956d
AnishPowar01 <powaranish74@gmail.com>
2023-08-13 18:50:13 +0530

    Added cart func

---
+++ src/components/core/Dashboard/Cart/RenderCartAmount.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RenderCartAmount = () => {
+  return (
+    <div>RenderCartAmount</div>
+  )
+}
+
+export default RenderCartAmount
---
+++ src/components/core/Dashboard/Cart/RenderCartCourse.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RenderCartCourse = () => {
+  return (
+    <div>RenderCartCourse</div>
+  )
+}
+
+export default RenderCartCourse
